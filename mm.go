5127819232b44044af11f62a17053c220a57e091
nick comer <nick@comer.io>
2016-06-10 09:32:19 -0400

    refactoring mm

--- mm.go
+++ mm.go
@@ -3,59 +3,42 @@ package mm
 import (
   "io"
   "os"
-  flac "github.com/nkcmr/mm-flac"
-  mpeg "github.com/nkcmr/mm-mpeg"
 )
-
-type nilExtractor struct {}
 
 // Parse takes a filename and automatically looks up an extractor to use
 func Parse (rs io.ReadSeeker) (Metadata, error) {
   extractor, err := initExtractor(rs)
   if err != nil {
-    return Metadata{}, err
+    var m Metadata
+    return m, err
   }
   return ParseWithExtractor(rs, extractor)
 }
 
 // ParseWithExtractor will extract music metadata out of a file with a specific
 // extractor
-func ParseWithExtractor (rs io.ReadSeeker, extractor Extractor) (Metadata, error) {
-  return Metadata{}, nil
-}
-
-func pickExtractor (rs io.ReadSeeker) (string, error) {
-  extractors := map[string]func (r io.ReadSeeker) (bool, error){
-    "flac": flac.CanExtract,
-    "mpeg": mpeg.CanExtract,
+func ParseWithExtractor (rs io.ReadSeeker, ex Extractor) (Metadata, error) {
+  if _, err := rs.Seek(0, os.SEEK_SET); err != nil {
+    return Metadata{}, err
   }
-  for _type, canExtract := range extractors {
-    _, err := rs.Seek(0, os.SEEK_SET)
-    if err != nil {
-      return "", err
-    }
-    ok, err := canExtract(rs)
-    if err != nil {
-      return "", err
-    }
-    if ok {
-      return _type, nil
-    }
-  }
-  return "", nil
+  return ex.Extract(rs)
 }
 
 func initExtractor (rs io.ReadSeeker) (Extractor, error) {
-  ex, err := pickExtractor(rs)
+  var ex Extractor
+  var err error
+  var choice string
+  choice, err = DetectorAudioType(rs)
   if err != nil {
-    return nilExtractor{}, err
+    return ex, err
   }
-  switch ex {
+  switch choice {
   case "flac":
-    return flac.NewExtractor(), nil
+    ex = FlacExtractor{}
   case "mpeg":
-    return mpeg.NewExtractor(), nil
+    ex = MpegExtractor{}
   default:
-    return nilExtractor{}, &MusicMetadataError{E_UNSUPPORTED_TYPE, "unsupported audio type"}
+    err = &MusicMetadataError{E_UNSUPPORTED_TYPE, "unsupported audio type"}
   }
+  return ex, err
 }
