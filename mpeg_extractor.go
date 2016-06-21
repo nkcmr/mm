9d24ddd19d1a2d28fd768f8c473700af5083f8ca
nick comer <nick@comer.io>
2016-06-20 22:57:35 -0400

    stile don't think the tag size is being read correctly 😒

--- mpeg_extractor.go
+++ mpeg_extractor.go
@@ -43,17 +43,19 @@ func (h id3Header) String() string {
     "flags:",
     fmt.Sprintf("  %s", h.flags),
     fmt.Sprintf("size: %d", h.size),
+    "",
   }, "\n")
 }
 
-func readHeader(data []byte) (h id3Header) {
+func readHeader(data []byte) *id3Header {
+  h := new(id3Header)
   h.version = id3Version{major: uint8(data[0]), minor: uint8(data[1])}
   h.flags = id3Flags{}
   flagsByte := uint8(data[2])
   h.flags.unsynchronisation = (flagsByte & 0x80) != 0
   h.flags.extendedHeader = (flagsByte & 0x40) != 0
   h.flags.experimental = (flagsByte & 0x20) != 0
-  h.size = uint32(((data[6] & 0xFF) | ((data[5] & 0xFF) << 7) | ((data[4] & 0xFF) << 14) | ((data[3] & 0xFF) << 21)) + 10)
+  h.size = uint32((data[3] << 21) | (data[4] << 14) | (data[5] << 7) | data[3])
   return h
 }
 
@@ -62,7 +64,7 @@ type MpegExtractor struct {}
 func (ex MpegExtractor) Extract(rs io.ReadSeeker) (*Metadata, error) {
   m := new(Metadata)
   rs.Seek(3, os.SEEK_SET)
-  header := make([]byte, 10)
+  header := make([]byte, 7)
   if _, err := io.ReadFull(rs, header); err != nil {
     return m, err
   }
@@ -70,6 +72,10 @@ func (ex MpegExtractor) Extract(rs io.ReadSeeker) (*Metadata, error) {
   if h.flags.extendedHeader {
 
   }
-  fmt.Println(h)
+  frames := make([]byte, h.size)
+  if _, err := io.ReadFull(rs, frames); err != nil {
+    return m, err
+  }
+  fmt.Println(*h, string(frames))
   return m, nil
 }
